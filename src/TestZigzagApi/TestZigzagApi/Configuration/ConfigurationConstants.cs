5b7016bd861549f0195aa4196f3f0fe1faf58251
Aleksei Koliada <a2.kolyada@itransition.com>
2021-12-21 21:31:08 +0200

    added spaces.

--- src/TestZigzagApi/TestZigzagApi/Configuration/ConfigurationConstants.cs
+++ src/TestZigzagApi/TestZigzagApi/Configuration/ConfigurationConstants.cs
@@ -3,7 +3,9 @@
     public static class ConfigurationConstants
     {
         public const string MondoDbSectionName = "MongoDatabase";
+        
         public const string AuthOptionsSectionName = "AuthOptions";
+        
         public const string DefaultCorsPolicyName = "DefaultCors";
     }
 }
